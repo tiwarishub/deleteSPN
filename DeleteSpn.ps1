314a73c1902f1161f89576a0fa717deeb85a948f
Vineet Mimrot <vimimrot@microsoft.com>
2019-01-09 13:35:37 +0530

    First Commit

---
+++ DeleteSpn.ps1
@@ -0,0 +1,14 @@
+﻿if ([string]::IsNullOrEmpty($(Get-AzureRmContext).Account)) {Login-AzureRmAccount}
+
+$Prefix = Read-Host -Prompt 'Input service principal prefix you want to delete'
+
+$applications = AzureRmADServicePrincipal -SearchString $Prefix;
+if (!$applications) {
+    Write-Host "No application found having prefix '$Prefix'"
+    exit;
+}
+$appName = ($applications.DisplayName | ForEach-Object { "{0}`n" -f $_});
+$confirm = Read-Host -Prompt "Following applications will be deleted. `n $appName `nPress 'Y' to confirm.";
+if ($confirm -eq 'Y') {
+    $applications.Id | ForEach-Object { Remove-AzureRmADServicePrincipal -Force -ObjectId $_ }
+}
