09498935f99823d2f5faa120851888a62f0f31ba
Vineet <vineet.mimrot@gmail.com>
2019-09-26 14:19:21 +0530

    Complete delete SPN

--- DeleteSpn.ps1
+++ DeleteSpn.ps1
@@ -1,9 +1,11 @@
-﻿if ([string]::IsNullOrEmpty($(Get-AzureRmContext).Account)) {Login-AzureRmAccount}
+$graphToken = "Bearer <Token>"
+
+if ([string]::IsNullOrEmpty($(Get-AzureRmContext).Account)) {Login-AzureRmAccount}
 
 $Prefix = Read-Host -Prompt 'Input service principal prefix you want to delete'
 
 if ($Prefix) {
-    $applications = AzureRmADServicePrincipal -SearchString $Prefix;
+    $applications = Get-AzureRmADApplication -DisplayNameStartWith $Prefix
 } else {
     Write-Host "No prefix provided, exiting."
     exit;
@@ -15,5 +17,10 @@ if (!$applications) {
 $appName = ($applications.DisplayName | ForEach-Object { "{0}`n" -f $_});
 $confirm = Read-Host -Prompt "Following applications will be deleted. `n $appName `nPress 'Y' to confirm.";
 if ($confirm -eq 'Y') {
-    $applications.Id | ForEach-Object { Remove-AzureRmADServicePrincipal -Force -ObjectId $_ }
+    $applications.ObjectId | ForEach-Object { Remove-AzureRmADApplication -Force -ObjectId $_; }
+    Start-Sleep -s 120
+    $applications.ObjectId | ForEach-Object {
+        $uri = "https://graph.microsoft.com/beta/directory/deletedItems/$_"
+        Invoke-RestMethod -Method DELETE -Headers @{"Authorization"=$graphToken; "Accept"="application/json, text/plain, */*"} -Uri $uri
+    }
 }
